0bbe09def280f5e8b63c0ec4d4dadc02e9308331
Gabriel Starczewski <starczewski@objectivity.co.uk>
2020-03-13 09:28:40 +0100

    fixing paths

--- kubernetes/bin/start_test_from_script_params.sh
+++ kubernetes/bin/start_test_from_script_params.sh
@@ -3,21 +3,21 @@
 #It requires that you supply the path to the jmx file
 #After execution, test script jmx file may be deleted from the pod itself but not locally.
 
-working_dir="`pwd`"
+working_dir="$(pwd)"
 
 #Get namesapce variable
 tenant="$1"
 jmx="$2"
 scenario_dir=$working_dir/../../jmeter/as_jmx
-report_dir=$working_dir/../tmp/report
-
+local_report_dir=$working_dir/../tmp/report
+report_dir=report
 
 test_name="$(basename "$scenario_dir/$jmx")"
 #delete evicted pods first
 kubectl get pods --all-namespaces --field-selector 'status.phase==Failed' -o json | kubectl delete -f -
 #Get Master pod details
 
-master_pod=`kubectl get po -n $tenant | grep Running | grep jmeter-master | awk '{print $1}'`
+master_pod=$(kubectl get po -n $tenant | grep Running | grep jmeter-master | awk '{print $1}')
 
 kubectl cp "$scenario_dir/$jmx" -n $tenant "$master_pod:/$test_name"
 
@@ -30,9 +30,10 @@ jmeter_args=$3
 echo "Threads $threads"
 echo "Report dir $report_dir"
 echo "Jmeter args $jmeter_args"
+ó
 kubectl exec -ti -n $tenant $master_pod -- rm -Rf "$tmp"
 kubectl exec -ti -n $tenant $master_pod -- mkdir -p "$tmp/$report_dir"
 kubectl exec -ti -n $tenant $master_pod -- /bin/bash /load_test "$test_name -l $tmp/results.csv -e -Gthreads=$threads $jmeter_args -o $tmp/$report_dir"
-kubectl cp "$tenant/$master_pod:$tmp/$report_dir" "$report_dir"
+kubectl cp "$tenant/$master_pod:$tmp/$report_dir" "$local_report_dir"
 ls -alh
 pwd
