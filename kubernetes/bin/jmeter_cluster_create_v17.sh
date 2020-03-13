72b53d627812ce536f961748b918674a761e8d9f
Gabriel Starczewski <starczewski@objectivity.co.uk>
2020-03-13 13:46:06 +0100

    added LoadBalancer removal

--- kubernetes/bin/jmeter_cluster_create_v17.sh
+++ kubernetes/bin/jmeter_cluster_create_v17.sh
@@ -10,7 +10,7 @@ kubectl delete namespace $tenant ||:
 echo "Replacing API version in deployment files:  apiVersion: apps/v1beta2 - > apiVersion: apps/v1"
 sed -i 's+apps/v1beta2+apps/v1+g' ../config/deployments/*_deploy.yaml
 
-echo "Removing Grafana LoadBalancer for local set-up"
+echo "Removing Grafana LoadBalancer for local set-up. Keep it on when running on Azure"
 sed -i 's+type: LoadBalancer++g' ../config/deployments/jmeter_grafana_svc.yaml
 
 
