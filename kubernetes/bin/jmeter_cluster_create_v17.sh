c3f41402d97c59721272a65bb7d340be998c09c4
Gabriel Starczewski <starczewski@objectivity.co.uk>
2020-03-16 13:32:51 +0100

    Grafan available at the port 30000

--- kubernetes/bin/jmeter_cluster_create_v17.sh
+++ kubernetes/bin/jmeter_cluster_create_v17.sh
@@ -12,7 +12,7 @@ echo "Replacing API version in deployment files:  apiVersion: apps/v1beta2 - > a
 sed -i 's+apps/v1beta2+apps/v1+g' ../config/deployments/*_deploy.yaml
 
 echo "Removing Grafana LoadBalancer for local set-up. Keep it on when running on Azure"
-sed -i 's+type: LoadBalancer++g' ../config/deployments/jmeter_grafana_svc.yaml
+sed -i 's+type: LoadBalancer+NodePort+g' ../config/deployments/jmeter_grafana_svc.yaml
 
 
 echo "Setting up cluster"
