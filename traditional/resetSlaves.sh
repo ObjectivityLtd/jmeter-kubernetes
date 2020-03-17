8528afe9a7dd7718c2ca66fea06f0f0f695bd33b
Gabriel Starczewski <starczewski@objectivity.co.uk>
2020-03-17 14:49:53 +0100

    moving loops to scripts

--- traditional/resetSlaves.sh
+++ traditional/resetSlaves.sh
@@ -6,11 +6,11 @@ reset_slaves() {
   local server_xms=$3
   local server_xmx=$4
   shift 4
-  local servers=("$@")
+  local servers=( "$@" )
   echo "Servers: $servers"
   for host in "${servers[@]}"; do
     command="curl -s -o /dev/null -w '%{http_code}' -X GET http://${host}:${flask_control_port}/restart/${jmeter_server_version}/${server_xms}/${server_xmx}"
-    echo "c: ${command}"
+    echo "${command}"
     http_code=$(command)
     if [ "$http_code" != "200" ]; then
     echo "Operation failed with code:  $http_code"
