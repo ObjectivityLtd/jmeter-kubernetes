535c3e2601d5729c4661489c6e3285ceab707c4d
Gabriel Starczewski <starczewski@objectivity.co.uk>
2020-03-17 14:41:43 +0100

    moving loops to scripts

--- traditional/resetSlaves.sh
+++ traditional/resetSlaves.sh
@@ -24,7 +24,7 @@ flask_control_port=$1
 jmeter_server_version=$2
 server_xms=$3
 server_xmx=$4
-jmeter_servers=$5
+jmeter_servers=$@
 
 echo "jmeter xmx: ${server_xmx}"
 echo "jmeter servers: ${jmeter_servers}"
