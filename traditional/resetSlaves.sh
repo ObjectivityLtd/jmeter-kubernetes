72e20ac043b1844fd867b495596572e98b789e99
Gabriel Starczewski <starczewski@objectivity.co.uk>
2020-03-18 10:42:21 +0100

    adding curl basic auth for jmeter controller

--- traditional/resetSlaves.sh
+++ traditional/resetSlaves.sh
@@ -8,12 +8,13 @@ reset_slaves() {
   local jmeter_server_version=$2
   local server_xms=$3
   local server_xmx=$4
-  shift 4
+  local curl_basic_auth=$5
+  shift 5
   #string to list
   IFS=' ' read -r -a servers <<< "$@"
   echo "Servers parsed: ${servers[@]}"
   for host in "${servers[@]}"; do
-    command="curl -s -o /dev/null -w \"%{http_code}\" -X GET http://${host}:${flask_control_port}/restart/${jmeter_server_version}/${server_xms}/${server_xmx}"
+    command="curl --user ${curl_basic_auth} -s -o /dev/null -w \"%{http_code}\" -X GET http://${host}:${flask_control_port}/restart/${jmeter_server_version}/${server_xms}/${server_xmx}"
     echo "${command}"
     http_code=$(echo "$command" | bash)
     if [ "$http_code" != "200" ]; then
@@ -28,10 +29,11 @@ flask_control_port=$1
 jmeter_server_version=$2
 server_xms=$3
 server_xmx=$4
-shift 4
+curl_basic_auth=$5
+shift 5
 jmeter_servers=$@
 
 echo "jmeter xmx: ${server_xmx}"
 echo "jmeter servers: ${jmeter_servers}"
 
-reset_slaves "${flask_control_port}" "${jmeter_server_version}" "${server_xms}" "${server_xmx}" "${jmeter_servers[@]}"
+reset_slaves "${flask_control_port}" "${jmeter_server_version}" "${server_xms}" "${server_xmx}" "${curl_basic_auth}" "${jmeter_servers[@]}"
--- traditional/stopSlaves.sh
+++ traditional/stopSlaves.sh
@@ -6,12 +6,13 @@
 
 reset_slaves() {
   local flask_control_port=$1
-  shift 1
+  local curl_basic_auth=$2
+  shift 2
   #string to list
   IFS=' ' read -r -a servers <<<"$@"
   echo "Servers parsed: ${servers[@]}"
   for host in "${servers[@]}"; do
-    command="curl -s -o /dev/null -w \"%{http_code}\" -X GET http://${host}:${flask_control_port}/stop"
+    command="curl --user ${curl_basic_auth} -s -o /dev/null -w \"%{http_code}\" -X GET http://${host}:${flask_control_port}/stop"
     echo "${command}"
     http_code=$(echo "$command" | bash)
     if [ "$http_code" != "200" ]; then
@@ -23,8 +24,9 @@ reset_slaves() {
 }
 
 flask_control_port=$1
-shift 1
+curl_basic_auth=$2
+shift 2
 jmeter_servers=$@
 echo "jmeter servers: ${jmeter_servers}"
 
-reset_slaves "${flask_control_port}" "${jmeter_servers[@]}"
+reset_slaves "${flask_control_port}" "${curl_basic_auth}" "${jmeter_servers[@]}"
