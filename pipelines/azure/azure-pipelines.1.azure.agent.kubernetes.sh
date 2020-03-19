93f2328909226b36ee7aba6b103c2bd4315d2c58
Gabriel Starczewski <starczewski@objectivity.co.uk>
2020-03-19 15:04:01 +0100

    update auto provisioning of service connection

--- pipelines/azure/azure-pipelines.1.azure.agent.kubernetes.sh
+++ pipelines/azure/azure-pipelines.1.azure.agent.kubernetes.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 #How to prepare all necessary azure resources to keep cost under control ?
 #Note: all of this can also be done in UI, howver for automartion I am presenting a CLI version.
+#edit this file, commit, in your devops org create PAT and make it available as env variable $pat
 #execute this in CLI:
 #cd ~ && rm -Rf jmeter-kubernetes && git clone https://github.com/ObjectivityLtd/jmeter-kubernetes && cd jmeter-kubernetes/pipelines/azure && chmod +x *.sh && ./azure-pipelines.1.azure.agent.kubernetes.sh
 
-#config
+#config cluster
 group_name=jmeter-group
 location=uksouth
 cluster_name=jubernetes
@@ -12,8 +13,15 @@ cluster_namespace=jmeter
 kubernetes_version=1.15.10
 node_size=Standard_D2_v2
 node_count=3
+#for test
 home_dir=$(pwd)
 test_jmx="cloudssky.jmx"
+#your devops details
+devops_org=gstarczewski
+devops_project=jmeter
+devops_service_connection_name=k8c
+devops_user=gstarczewski
+devops_pat=calzm6eokgoy7m3m54ikqkocdzqfxskaw6o2h3al34wgj4jngoxa
 
 #help functions
 
@@ -43,9 +51,53 @@ wait_for_pods(){
   done
 
 }
+
+delete_service_connection(){
+  local org=$1
+  local project=$2
+  local user=$3
+  local pat=$4
+  local service_connection_name=$5
+  silent=" -s -o /dev/null"
+  verbose=" -v"
+  opts="$silent"
+
+  service_connection_id=$(curl --user $user:$pat https://dev.azure.com/$org/project/_apis/serviceendpoint/endpoints?endpointNames=${service_connection_name} | jq '.value[0].id' | sed "s/\"//g" )
+ echo "id: $service_connection_id"
+ if [ -z "$service_connection_id" ]; then
+        echo "Cannot get id. skipping connection deletion"
+        return
+ fi
+
+ http_code=$(curl $opts -w "%{http_code}"  --user $user:$pat -X DELETE https://dev.azure.com/$org/$project/_apis/serviceendpoint/endpoints/${service_connection_id}?api-version=5.1-preview.2)
+
+  echo "Http code: $http_code"
+  if [ "$http_code" != "204" ]; then
+      echo "Connection $service_connection_name by id $service_connection_id was not deleted."
+  else
+      echo "Connection $service_connection_name was deleted. "
+  fi
+
+}
+
+create_service_connection(){
+  echo "Creating connection"
+  cluster_id=$(az aks show --name jubernetes --resource-group jmeter-group | jq '.id')
+  echo "Cluster id $cluster_id"
+  cat template.json | \
+                sed "s/#azureSubscriptionId/dupa/g" |\
+                sed "s/#clusterId/$cluster_id/g" |\
+                sed "s/#namespace/$namespace/g" \
+  > payload.json
+  cat payload.json
+  http_code=$(curl $silent -w "$%{http_code}" --user gstarczewski:$pat -X POST -H "Content-Type: application/json" -d @payload.json https://dev.azure.com/gstarczewski/jmeter/_apis/serviceendpoint/endpoints?api-version=5.0-preview.2)
+  echo "Http code: $http_code"
+}
 #PART 1 - create a kubernetes cluster in Azure, execute as one command:
 
-#1. Open Azure CLI
+#1. Delete service connection if exists
+echo "Deleting k8 service connection $devops_service_connection_name if exists"
+delete_service_connection $devops_org $devops_project $devops_user calzm6eokgoy7m3m54ikqkocdzqfxskaw6o2h3al34wgj4jngoxa $devops_service_connection_name
 #2. Delete entire resource group if exist:
 echo "Deleting group $group_name if exists"
 az group delete -n "$group_name" --yes || :
